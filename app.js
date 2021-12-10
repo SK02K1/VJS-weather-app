aec2d2c7980d8e61152fc37654f8825072e94b95
SK02K1 <ksourabh458@gmail.com>
2021-12-10 23:00:21 +0530

    style: add weather card

--- app.js
+++ app.js
@@ -10,16 +10,17 @@ const getData = async (city) => {
     return {cityData, weatherData};
 };
 
+
 const updateUI = (data) => {
     const {EnglishName} = data.cityData;
     const {IsDayTime, WeatherText, WeatherIcon, Temperature: { Metric: {Value} }} = data.weatherData;
-    weatherCard.innerHTML = (`
-    name: ${EnglishName}
-    isDayTime: ${IsDayTime}
-    weatherText: ${WeatherText}
-    weatherIcon: ${WeatherIcon}
-    temperature: ${Value} °C
-    `);
+    weatherCard.innerHTML = `
+    <img src="./img/${IsDayTime ? "day" : "night"}.svg" class="img-day-time" alt="day-time-illustration" />
+    <h2>${EnglishName}</h2>
+    <div>${WeatherText}</div>
+    <img src="./img/icons/${WeatherIcon}.svg" class="img-weather-icon" alt="weather-icon" />
+    <div>${Value} °C</div>
+    `;
 };
 
 
